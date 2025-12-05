e1f993537a2aa9eefe156984c27f603549c99ad8
SemenovvAG <semenov.anrdey@gmail.com>
2025-12-05 16:10:09 +0300

    Первая правка

--- main.go
+++ main.go
@@ -4,17 +4,15 @@ import "fmt"
 
 func main() {
 	var userEnt int
-	var usd float64 =
-	var eur float64 =
-	var rub float64 =
-
+	var usd float64 = 76.5
+	var eur float64 = 34.3
+	var rub float64 = 23.6
+	reading := enter(userEnt)
 }
 func enter(userEnt int) int {
 	fmt.Scan(userEnt)
-reading := fmt.Sprintf()
-return reading
+	reading := fmt.Sprintf()
+	return reading
 }
-func calc(int, float64,float64) float64{
-	
-	
+func calc(int, string, string) float64 {
 }
