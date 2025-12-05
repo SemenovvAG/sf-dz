914dd40fdaf83b20f204944235039126f7debec8
SemenovvAG <semenov.anrdey@gmail.com>
2025-12-05 19:03:35 +0300

    Начал работу над калькулятором валют

--- main.go
+++ main.go
@@ -2,17 +2,24 @@ package main
 
 import "fmt"
 
+func input() (int, string, string) {
+	var userInput int
+	var usd float64 = 76.09
+	var euro float64 = 88.70
+	fmt.Print("=Калькулятор курса валют=")
+	fmt.Println("Введите колличество рублей")
+	fmt.Scan(&userInput)
+	fmt.Print("Введите валюту из которой будете конвертровать(usd, euro)")
+	fmt.Scan(&usd)
+	fmt.Print("Введите валюту в которую будете конвертировать(usd, euro)")
+	fmt.Scan(&euro)
+	return userInput, usd, euro
+}
+
 func main() {
-	var userEnt int
-	var usd float64 = 76.5
-	var eur float64 = 34.3
-	var rub float64 = 23.6
-	reading := enter(userEnt)
+
+	reading := input(userInput)
 }
-func enter(userEnt int) int {
-	fmt.Scan(userEnt)
-	reading := fmt.Sprintf()
-	return reading
-}
+
 func calc(int, string, string) float64 {
 }
