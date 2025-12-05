4d0dbdcfcee2019d0e424cb5e4993968a84b85d9
SemenovvAG <semenov.anrdey@gmail.com>
2025-12-05 15:15:00 +0300

    Начал делать домашнее задание

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var userEnt int
+	var usd float64 =
+	var eur float64 =
+	var rub float64 =
+
+}
+func enter(userEnt int) int {
+	fmt.Scan(userEnt)
+reading := fmt.Sprintf()
+return reading
+}
+func calc(int, float64,float64) float64{
+
+	
+}
