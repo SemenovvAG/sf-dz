09b33632fac169e2fa3bf12fa7afdf6b851849a8
SemenovvAG <semenov.anrdey@gmail.com>
2025-12-23 18:04:29 +0300

    23.12.2025

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func main() {
 	for {
-		userInput, from, in := reading()
-		result := calculateImp(userInput, from, in)
+		from, userInput, in := reading()
+
+		result := calculateImp(from, userInput, in)
+		
 		fmt.Println(result)
 		isRepeatCalculation := checkRepeatCalculate()
 		if !isRepeatCalculation {
@@ -14,36 +16,49 @@ func main() {
 	}
 
 }
-func reading() (int, string, string) {
+func reading() (string, int, string) errors {
 
 	var userInput int
 	var from string
 	var in string
-
 	fmt.Println("=Калькулятор курса валют=")
-	fmt.Print("Введите количество: ")
-	fmt.Scan(&userInput)
-
-	fmt.Print("Введите валюту из которой будете конвертировать (usd, euro): ")
-	fmt.Scan(&from)
-
+	for {for {
+		fmt.Print("Введите валюту из которой будете конвертировать (usd, euro): ")
+		fmt.Scan(&from)
+		if from == "usd" || from == "eur" {
+			break
+		} else {
+			return 0 , errors.New("Ошибка, введите usd или eur: ")
+		}
+	}
+	for {
+		fmt.Print("Введите количество: ")
+		fmt.Scan(&userInput)
+		if userInput >= 0 {
+			break
+		} else {
+			return 0, errors.New("Ошибка, введите число больше 0: ")
+		}
+	} 
 	fmt.Print("Введите валюту в которую будете конвертировать (usd, euro): ")
 	fmt.Scan(&in)
-	return userInput, from, in
+	return from, userInput, in
 }
+}
+	
 
-func calculateImp(userInput int, from string, in string) float64 {
+func calculateImp(from string, userInput int, in string) float64 {
 
 	var result float64
 	courseUsd := 76.1
-	courseEuro := 86.3
+	courseEur := 86.3
 
-	if from == "usd" && in == "euro" {
-		result = float64(userInput) * courseUsd / courseEuro
-	} else if from == "euro" && in == "usd" {
-		result = float64(userInput) * courseEuro / courseUsd
+	if from == "usd" && in == "eur" {
+		result = float64(userInput) * courseUsd / courseEur
+	} else if from == "eur" && in == "usd" {
+		result = float64(userInput) * courseEur / courseUsd
 	} else {
-		fmt.Println("Неправильно введена валюта")
+		return 0
 	}
 	return result
 }
@@ -56,3 +71,4 @@ func checkRepeatCalculate() bool {
 	}
 	return false
 }
+func checkReadingFrom() bool
